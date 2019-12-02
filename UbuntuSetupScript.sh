d13c37fe78a147667a012bff25fcfddf1f335975
Brendan Austin <vk2fbha@gmail.com>
2019-12-02 12:42:46 +1100

    Update UbuntuSetupScript.sh

--- UbuntuSetupScript.sh
+++ UbuntuSetupScript.sh
@@ -5,4 +5,8 @@ sudo apt-get install firefox
 sudo pip install selenium
 wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz 
 sudo sh -c 'tar -x geckodriver -zf geckodriver-v0.26.0-linux64.tar.gz -O > /usr/bin/geckodriver' 
-sudo chmod +x /usr/bin/geckodriver rm geckodriver-v0.26.0-linux64.tar.gz
+sudo chmod +x /usr/bin/geckodriver 
+rm geckodriver-v0.26.0-linux64.tar.gz
+wget https://raw.githubusercontent.com/pingu2211/TechClicker/master/TechClicker.sh
+sudo chmod +x ./TechClicker.py 
+nohup python3 TechClicker.py 
